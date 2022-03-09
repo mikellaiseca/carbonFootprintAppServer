d49950ace6c4c1bcd3b9bbf2c1f86d63e4523ad5
mikellaiseca <mikellaiseca@gmail.com>
2022-03-09 09:49:41 +0100

    delete footprints

--- routes/footprint.routes.js
+++ routes/footprint.routes.js
@@ -12,7 +12,6 @@ router.post('/flight-form', isAuthenticated, (req, res) => {
     const { _id } = req.payload
 
     const { passengers, departure_airport, destination_airport } = req.body
-    console.log(req.body)
 
     footprintService
         .getFlightFootprint(passengers, departure_airport, destination_airport)
@@ -20,7 +19,6 @@ router.post('/flight-form', isAuthenticated, (req, res) => {
             res.json(response.data)
 
             const { passengers, legs, distance_value, distance_unit, carbon_kg } = response.data.data.attributes
-            console.log(response.data.data.attributes)
 
             FootprintFlight
                 .create({ user: _id, passengers, legs, distance_value, distance_unit, carbon_kg })
@@ -42,25 +40,19 @@ router.post('/car-form', isAuthenticated, (req, res) => {
         .then(response => {
             res.json(response.data)
 
-
             const { distance_value, vehicle_model, vehicle_make, vehicle_year, distance_unit, carbon_kg } = response.data.data.attributes
 
             FootprintCar
                 .create({ user: _id, distance_value, vehicle_model, vehicle_make, vehicle_year, distance_unit, carbon_kg })
 
-
         })
         .catch(err => console.log(err))
-
 
 })
 
 router.get("/car-custom-footprints/:userId", (req, res) => {
 
-
     const { userId } = req.params
-
-    console.log(userId, 'user BAAAAAAAAAAAAAAACK')
 
     FootprintCar
         .find({ user: userId })
@@ -71,10 +63,7 @@ router.get("/car-custom-footprints/:userId", (req, res) => {
 
 router.get("/flight-custom-footprints/:userId", (req, res) => {
 
-
     const { userId } = req.params
-
-    console.log(userId, 'user BAAAAAAAAAAAAAAACK')
 
     FootprintFlight
         .find({ user: userId })
@@ -83,13 +72,24 @@ router.get("/flight-custom-footprints/:userId", (req, res) => {
 
 })
 
+router.delete('/delete-footprintCar/:footprintCarId', (req, res) => {
 
-//     const { id } = req.params
+    const { footprintCarId } = req.params
 
-//     FootprintCar
-//         .findByIdAndDelete(id)
-//         .then(response => res.json(response))
-//         .catch(err => res.status(500).json(err))
-// })
+    FootprintCar
+        .findByIdAndDelete(footprintCarId)
+        .then(response => res.json(response))
+        .catch(err => res.status(500).json(err))
+})
+
+router.delete('/delete-footprintFlight/:footprintFlightId', (req, res) => {
+
+    const { footprintFlightId } = req.params
+
+    FootprintFlight
+        .findByIdAndDelete(footprintFlightId)
+        .then(response => res.json(response))
+        .catch(err => res.status(500).json(err))
+})
 
 module.exports = router
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -24,40 +24,4 @@ router.get("/users/:_id", (req, res) => {
         .catch(err => res.status(500).json(err))
 })
 
-router.put(':user_id/push-comment-post', (req, res) => {
-
-    const { user_id } = req.params
-    console.log(comments)
-
-    User
-        .findByIdAndUpdate(user_id, { $push: { comments: req.body } })
-        .then(response => res.json(response))
-        .catch(err => res.status(500).json(err))
-})
-
-router.put('/:comment_id/pull-comment-post', isAuthenticated, (req, res) => {
-
-    const { comment_id } = req.params
-
-    const { _id } = req.payload
-
-    User
-        .findByIdAndUpdate(_id, { $pull: { comments: comment_id } }, { new: true })
-        .then(response => { res.json(response) })
-        .catch(err => res.status(500).json(err))
-})
-
-router.put('/push-footprintCar', isAuthenticated, (req, res) => {
-
-    const { _id } = req.payload
-    console.log(_id, 'soy la creacion en el back route')
-    console.log(req.body, 'soy la creacion creada el REQ BODY')
-    console.log(savedFootprintCar)
-
-    User
-        .findByIdAndUpdate(_id, { $push: { savedFootprintCar: req.body } })
-        .then(response => console.log(response))
-        .catch(err => res.status(500).json(err))
-})
-
 module.exports = router
